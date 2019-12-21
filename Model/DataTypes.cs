0c02a7b05f0114ed37e596dabb24c64893e581e8
agcharis67705 <agcharis67705@th-nuernberg.de>
2019-12-21 18:05:22 +0100

    initial commit

---
+++ Model/DataTypes.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ChessDirectedGraphSimulation
+{
+    public class ChessPosition
+    {
+        public int X { get; }
+        public int Y { get; }
+        public string ChessX
+        {
+            get
+            {
+                return 
+                (X == 1) ? "A" :
+                (X == 2) ? "B" :
+                (X == 3) ? "C" :
+                (X == 4) ? "D" :
+                (X == 5) ? "E" :
+                (X == 6) ? "F" :
+                (X == 7) ? "G" :
+                (X == 8) ? "H" :
+                "fuck";
+            }
+        }
+        public ChessPosition(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"({ChessX},{Y})";
+        }
+    }
+
+    public class Node
+    {
+        public string Name { get; }
+        public ChessPosition Position { get; }
+        public Node(string name, ChessPosition position)
+        {
+            Name = name;
+            Position = position;
+        }
+
+        public bool EqualsNode(Node other)
+        {
+            return Position.X == other.Position.X && Position.Y == other.Position.Y;
+        }
+
+        public override string ToString()
+        {
+            return $"Node{Name.ToString()}\t@ {Position.ToString()}";
+        }
+    }
+
+    public class AdjecencyList
+    {
+        private readonly Dictionary<Node, IEnumerable<Node>> adjacencyList;
+
+        public AdjecencyList()
+        {
+            adjacencyList = new Dictionary<Node, IEnumerable<Node>>();
+        }
+
+        public void AddEntry(Node from, IEnumerable<Node> to)
+        {
+            adjacencyList.Add(from, to);
+        }
+
+        public bool HasEntryFor(Node from)
+        {
+            return adjacencyList.Keys.Where(node => from.EqualsNode(node)).Any();
+        }
+
+        public override string ToString()
+        {
+            string output = string.Empty;
+
+            foreach (var entry in adjacencyList)
+            {
+                output += $"{entry.Key.ToString()}\t->\t";
+                foreach (var target in entry.Value)
+                {
+                    output += $"{target.ToString()}, ";
+                }
+                output = output.Remove(output.Length - 2);
+                output += System.Environment.NewLine;
+            }
+
+            return output;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChessDirectedGraphSimulation
+{
+    class Program
+    {
+        public static int nodeCounter = 1;
+        public static List<Node> nodes = new List<Node>();
+
+        static void Main(string[] args)
+        {
+            ChessPosition startPosition = new ChessPosition(3, 4);
+            var adjacencyList = GetChessGraphAsAdjacencyList(KnightMovesAsNodes, startPosition);
+
+            Console.WriteLine(adjacencyList.ToString());
+        }
+
+        public static AdjecencyList GetChessGraphAsAdjacencyList(Func<Node, IEnumerable<Node>> DiscoverPossibleMovesAsNodes, ChessPosition chessPieceStartPosition)
+        {
+            AdjecencyList adjacencyList = new AdjecencyList();
+
+            Queue<Node> nodesToProcess = new Queue<Node>();
+            Node startNode = NewNode(chessPieceStartPosition);
+
+            nodesToProcess.Enqueue(startNode);
+
+            while(nodesToProcess.Count != 0)
+            {
+                Node sourceNode = nodesToProcess.Dequeue();
+
+                if (!adjacencyList.HasEntryFor(sourceNode)) {
+                    IEnumerable<Node> targetNodes = DiscoverPossibleMovesAsNodes(sourceNode);
+                    adjacencyList.AddEntry(sourceNode, targetNodes);
+                    foreach (var node in targetNodes) nodesToProcess.Enqueue(node);
+                }
+            }
+
+            return adjacencyList;
+        }
+
+        public static Node NewNode(ChessPosition position)
+        {
+            string name = (nodeCounter++).ToString();
+            Node newNode = new Node(name, position);
+            nodes.Add(newNode);
+            return newNode;
+        }
+
+        public static IEnumerable<Node> KnightMovesAsNodes(Node startingNode)
+        {
+            List<Node> movesAsNodes = new List<Node>();
+            ChessPosition pos = startingNode.Position;
+
+            ChessPosition testPos = new ChessPosition(pos.X + 1, pos.Y + 2);
+            if (InRange(testPos))
+            {
+                (bool exists, Node node) = NodeForPosExists(testPos);
+                if(exists) movesAsNodes.Add(node);
+                else movesAsNodes.Add(NewNode(testPos));
+            }
+
+            testPos = new ChessPosition(pos.X + 2, pos.Y + 1);
+            if (InRange(testPos))
+            {
+                (bool exists, Node node) = NodeForPosExists(testPos);
+                if (exists) movesAsNodes.Add(node);
+                else movesAsNodes.Add(NewNode(testPos));
+            }
+
+            testPos = new ChessPosition(pos.X + 2, pos.Y - 1);
+            if (InRange(testPos))
+            {
+                (bool exists, Node node) = NodeForPosExists(testPos);
+                if (exists) movesAsNodes.Add(node);
+                else movesAsNodes.Add(NewNode(testPos));
+            }
+
+            testPos = new ChessPosition(pos.X + 1, pos.Y - 2);
+            if (InRange(testPos))
+            {
+                (bool exists, Node node) = NodeForPosExists(testPos);
+                if (exists) movesAsNodes.Add(node);
+                else movesAsNodes.Add(NewNode(testPos));
+            }
+
+            testPos = new ChessPosition(pos.X - 1, pos.Y - 2);
+            if (InRange(testPos))
+            {
+                (bool exists, Node node) = NodeForPosExists(testPos);
+                if (exists) movesAsNodes.Add(node);
+                else movesAsNodes.Add(NewNode(testPos));
+            }
+
+            testPos = new ChessPosition(pos.X - 2, pos.Y - 1);
+            if (InRange(testPos))
+            {
+                (bool exists, Node node) = NodeForPosExists(testPos);
+                if (exists) movesAsNodes.Add(node);
+                else movesAsNodes.Add(NewNode(testPos));
+            }
+
+            testPos = new ChessPosition(pos.X - 2, pos.Y + 1);
+            if (InRange(testPos))
+            {
+                (bool exists, Node node) = NodeForPosExists(testPos);
+                if (exists) movesAsNodes.Add(node);
+                else movesAsNodes.Add(NewNode(testPos));
+            }
+
+            testPos = new ChessPosition(pos.X - 1, pos.Y + 2);
+            if (InRange(testPos))
+            {
+                (bool exists, Node node) = NodeForPosExists(testPos);
+                if (exists) movesAsNodes.Add(node);
+                else movesAsNodes.Add(NewNode(testPos));
+            }
+
+            return movesAsNodes;
+
+            bool InRange(ChessPosition position)
+            {
+                return position.X > 0 && position.X <= 8 && position.Y > 0 && position.Y <= 8;
+            }
+
+            (bool exists, Node existingNode) NodeForPosExists(ChessPosition positionToTest)
+            {
+                Node dummy = new Node("phyack", positionToTest);
+                var candidates = nodes.Where(node => dummy.EqualsNode(node));
+                if (candidates.Any())
+                {
+                    return (true, candidates.First());
+                }
+                else
+                {
+                    return (false, null);
+                }
+            }
+        }
+    }
+}
